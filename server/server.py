f1de041c6a634d5703907f600febe7d411775f67
quinnyyy <quinnyyy@bu.edu>
2019-11-02 13:01:16 -0400

    blah

--- server/server.py
+++ server/server.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_socketio import SocketIO
+from flask_cors import CORS
 import eventlet
 
 app = Flask(__name__)
+CORS(app)
 socketio = SocketIO(app)
 
 @app.route('/')
