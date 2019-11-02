6117fffee1308bb9a90e46d696d0b6496af2aaaf
quinnyyy <quinnyyy@bu.edu>
2019-11-02 13:16:54 -0400

    improved

--- server/server.py
+++ server/server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_socketio import SocketIO
 from flask_cors import CORS
 import eventlet
@@ -9,11 +9,19 @@ socketio = SocketIO(app)
 
 @app.route('/')
 def hello_world():
-    return "yooo"
+    return render_template('test.html')
 
 @socketio.on('create')
 def on_create():
     print("yooooooo")
+
+def messageReceived(methods=['GET', 'POST']):
+    print('message was received!!!')
+
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    print('received my event: '+ str(json))
+    socketio.emit('my response', json, callback=messageReceived)
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
