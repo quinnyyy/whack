a97eb7698724196bfd5d4a948f9620a1566c8558
quinnyyy <quinnyyy@bu.edu>
2019-11-02 12:40:28 -0400

    more server

--- server/server.py
+++ server/server.py
@@ -1,6 +1,17 @@
 from flask import Flask
+from flask_socketio import SocketIO
+import eventlet
+
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 @app.route('/')
 def hello_world():
-    return "Hello world"
+    return "yooo"
+
+@socketio.on('create')
+def on_create():
+    print("yooooooo")
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
